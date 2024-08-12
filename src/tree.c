ed6a644697368544eb35899c9d492de8925fad0d
André Meneses <andre_meneses2001@hotmail.com>
2024-08-11 22:41:16 -0300

    Umas coisas ai

--- src/tree.c
+++ src/tree.c
@@ -10,18 +10,30 @@
 #define score(G, H) (((G) * (G)) / ((H) + tree->lambda))
 #define weight(G, H) (-(G) / ((H) + tree->lambda))
 
-TreeNode *splitNode(Data *Xy, Subset *index_subset, int cur_depth,
-                    GradientPair *gpair, XGBoostTree *tree) {
+// 1º FUNÇÃO CRÍTICA
+#include <omp.h>
+
+typedef struct {
+    double score;
+    int feature;
+    double split;
+    Subset *subset;
+} BestData;
+
+#pragma omp declare reduction(maximum : BestData : omp_out = omp_in.score > omp_out.score ? omp_in : omp_out)
+
+TreeNode *splitNode(Data *Xy, Subset *index_subset, int cur_depth, GradientPair *gpair, XGBoostTree *tree) {
     double G = 0, H = 1e-16;
-    TreeNode *ret = mallocOrDie(sizeof(TreeNode));
+    TreeNode *ret = (TreeNode *)malloc(sizeof(TreeNode));
 
     #pragma omp parallel for reduction(+:G,H)
-    for (int i = 0; i < Xy->n_example; i++)
+    for (int i = 0; i < Xy->n_example; i++) {
         if (inSubset(i, index_subset)) {
             G += gpair[i].g;
             H += gpair[i].h;
         }
-    
+    }
+
     if (cur_depth == tree->max_depth) {
         ret->left = NULL;
         ret->right = NULL;
@@ -30,76 +42,62 @@ TreeNode *splitNode(Data *Xy, Subset *index_subset, int cur_depth,
     }
 
     double before_score = score(G, H);
+    Subset *next_subset = initSubset(index_subset->size, 0);
     Subset *best_next_subset = initSubset(index_subset->size, 0);
-    double best_score = 0;
-    int best_feature = -1;
-    double best_split = 0;
+    BestData best = {0, -1, 0.0, best_next_subset};
 
-    #pragma omp parallel
-    {
-        double local_best_score = 0;
-        double local_best_split = 0;
-        int local_best_feature = -1;
-        Subset *local_best_next_subset = initSubset(index_subset->size, 0);
+    // Parallel for each feature
+    #pragma omp parallel for schedule(dynamic) private(next_subset)
+    for (int i = 0; i < Xy->n_feature; i++) {
+        double G_L = 0, H_L = 0, G_R, H_R;
+        Subset *local_next_subset = initSubset(index_subset->size, 0);
+        resetSubset(local_next_subset);
 
-        #pragma omp for schedule(dynamic)
-        for (int i = 0; i < Xy->n_feature; i++) {
-            double G_L = 0, H_L = 0, G_R, H_R;
-            Subset *local_next_subset = initSubset(index_subset->size, 0);
-            resetSubset(local_next_subset);
+        for (int j = 0; j < Xy->n_example; j++) {
+            if (local_next_subset->cnt == index_subset->cnt - 1)
+                break;
 
-            for (int j = 0; j < Xy->n_example; j++) {
-                int example_index = Xy->feature_blocks[i][j];
-                if (!inSubset(example_index, index_subset))
-                    continue;
-                
-                addToSubset(example_index, local_next_subset);
-                G_L += gpair[example_index].g;
-                H_L += gpair[example_index].h;
-                G_R = G - G_L;
-                H_R = H - H_L;
-                double after_score = score(G_L, H_L) + score(G_R, H_R);
+            int example_index = Xy->feature_blocks[i][j];
 
-                if (after_score > local_best_score) {
-                    local_best_score = after_score;
-                    local_best_feature = i;
-                    local_best_split = Xy->X[example_index][i];
-                    memcpy(local_best_next_subset->bitset, local_next_subset->bitset,
-                           sizeof(char) * BITNSLOTS(local_next_subset->size));
-                    local_best_next_subset->cnt = local_next_subset->cnt;
+            if (!inSubset(example_index, index_subset))
+                continue;
+
+            addToSubset(example_index, local_next_subset);
+            G_L = gpair[example_index].g;
+            H_L = gpair[example_index].h;
+            G_R = G - G_L;
+            H_R = H - H_L;
+            double after_score = score(G_L, H_L) + score(G_R, H_R);
+
+            #pragma omp critical
+            {
+                if (after_score > best.score) {
+                    best.score = after_score;
+                    best.feature = i;
+                    best.split = Xy->X[example_index][i];
+                    memcpy(best.subset->bitset, local_next_subset->bitset, sizeof(char) * BITNSLOTS(local_next_subset->size));
+                    best.subset->cnt = local_next_subset->cnt;
                 }
             }
-            freeSubset(local_next_subset);
         }
-
-        #pragma omp critical
-        {
-            if (local_best_score > best_score) {
-                best_score = local_best_score;
-                best_feature = local_best_feature;
-                best_split = local_best_split;
-                memcpy(best_next_subset->bitset, local_best_next_subset->bitset,
-                       sizeof(char) * BITNSLOTS(local_best_next_subset->size));
-                best_next_subset->cnt = local_best_next_subset->cnt;
-            }
-        }
-        freeSubset(local_best_next_subset);
+        freeSubset(local_next_subset);
     }
 
-    if (best_score > before_score + tree->gamma) {
-        ret->feature_id = best_feature;
-        ret->info.split_cond = best_split;
-        ret->left = splitNode(Xy, best_next_subset, cur_depth + 1, gpair, tree);
-        best_next_subset->cnt = index_subset->cnt - best_next_subset->cnt;
-        for (int i = 0; i < BITNSLOTS(best_next_subset->size); i++)
-            best_next_subset->bitset[i] = index_subset->bitset[i] & ~(best_next_subset->bitset[i]);
-        ret->right = splitNode(Xy, best_next_subset, cur_depth + 1, gpair, tree);
+    if (best.score > before_score + tree->gamma) {
+        ret->feature_id = best.feature;
+        ret->info.split_cond = best.split;
+        ret->left = splitNode(Xy, best.subset, cur_depth + 1, gpair, tree);
+        best.subset->cnt = index_subset->cnt - best.subset->cnt;
+        for (int i = 0; i < BITNSLOTS(best.subset->size); i++)
+            best.subset->bitset[i] = index_subset->bitset[i] & ~(best.subset->bitset[i]);
+        ret->right = splitNode(Xy, best.subset, cur_depth + 1, gpair, tree);
     } else {
         ret->left = NULL;
         ret->right = NULL;
         ret->info.leaf_value = weight(G, H);
     }
-    freeSubset(best_next_subset);
+    freeSubset(next_subset);
+    freeSubset(best.subset);
     return ret;
 }
 
